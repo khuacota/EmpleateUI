0a2a084a87b74d08c09bbc26f01d32cfe6d67a48
david sanguino <sanguino.terrazas.david@gmail.com>
2018-10-05 12:13:17 -0400

    vista principal add

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,16 +3,26 @@ import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
 import { MaterialModule } from './modules/material/material.module';
+import { LayoutModule } from '@angular/cdk/layout';
+import { MatToolbarModule, MatButtonModule, MatSidenavModule, MatIconModule, MatListModule } from '@angular/material';
 import { BuscarEmpleadoComponent } from './components/buscar-empleado/buscar-empleado.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 @NgModule({
   declarations: [
     AppComponent,
-    BuscarEmpleadoComponent
+    BuscarEmpleadoComponent,
   ],
   imports: [
     BrowserModule,
     MaterialModule,
+    LayoutModule,
+    MatToolbarModule,
+    MatButtonModule,
+    MatSidenavModule,
+    MatIconModule,
+    MatListModule,
+    BrowserAnimationsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
