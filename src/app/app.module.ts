ce68ca803fdae96aac48a467037fb5b3e0928b55
Karem <karem.huacota@gmail.com>
2018-10-05 13:16:26 -0400

    menu and logo added

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,6 +5,7 @@ import { AppComponent } from './app.component';
 import { MaterialModule } from './modules/material/material.module';
 import { ProfesionService } from './services/profesiones/profesion.service.ts.service';
 import { BuscarEmpleadoComponent } from './components/buscar-empleado/buscar-empleado.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 @NgModule({
   declarations: [
@@ -14,6 +15,7 @@ import { BuscarEmpleadoComponent } from './components/buscar-empleado/buscar-emp
   imports: [
     BrowserModule,
     MaterialModule,
+    BrowserAnimationsModule,
   ],
   providers: [ProfesionService],
   bootstrap: [AppComponent]
--- src/app/components/buscar-empleado/buscar-empleado.component.ts
+++ src/app/components/buscar-empleado/buscar-empleado.component.ts
@@ -11,5 +11,8 @@ export class BuscarEmpleadoComponent implements OnInit {
 
   ngOnInit() {
   }
+  searchCourses(value: string) {
+
+  }
 
 }
