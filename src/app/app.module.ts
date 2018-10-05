2f2826b752eb293bf4c08e6ec1c6b6befc2dfc94
unknown <16844519+Drayko@users.noreply.github.com>
2018-10-05 12:28:29 -0400

    backup before update de master

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,9 +1,18 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
+import { FormsModule }    from '@angular/forms';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { Router } from '@angular/router';
+
 import { AppComponent } from './app.component';
 import { MaterialModule } from './modules/material/material.module';
 import { BuscarEmpleadoComponent } from './components/buscar-empleado/buscar-empleado.component';
+
+import { MatButtonModule, MatListModule, MatIconModule, MatCardModule, MatMenuModule, MatInputModule, MatButtonToggleModule,
+         MatProgressSpinnerModule, MatSelectModule, MatSlideToggleModule, MatDialogModule, MatSnackBarModule, MatToolbarModule,
+         MatTabsModule, MatSidenavModule, MatTooltipModule, MatRippleModule, MatRadioModule, MatGridListModule,
+         MatDatepickerModule, MatNativeDateModule, MatSliderModule, MatAutocompleteModule } from '@angular/material';
 
 @NgModule({
   declarations: [
