f2274a693a27bbc79ab87f4edad557cc876107a1
Beleenus <ni_rc@outlook.com>
2018-11-30 12:15:53 -0400

    fixs

--- src/app/components/buscar-empleado/employee-information/employee-information.component.ts
+++ src/app/components/buscar-empleado/employee-information/employee-information.component.ts
@@ -12,26 +12,44 @@ import { switchMap } from 'rxjs/operators';
   styleUrls: ['./employee-information.component.css']
 })
 export class EmployeeInformationComponent implements OnInit {
-  private basicInfo: BasicEmployee;
-  private academicInfo: Academic;
+  public basicInfo: BasicEmployee;
+  public academicInfo: Academic;
   private employeeId: string;
   constructor(private basicInfService: BasicInformationService, private academicService: AcademicService, private route: ActivatedRoute, private router: Router) {
     this.basicInfo = new BasicEmployee();
     this.academicInfo = new Academic();
-    this.basicInfService.getOne(this.employeeId).subscribe((res: BasicEmployee) => {
-      this.basicInfo = res;
-    });
-    this.academicService.getOne(this.employeeId).subscribe((res: Academic) => {
-      this.academicInfo = res;
-    });
-  }
-
-  ngOnInit() {
+    this.employeeId = '1';
     this.route.paramMap.pipe(
       switchMap((params: ParamMap) =>
         this.employeeId = params.get('id')
       )
     );
+    
+  }
+
+  showNull(data) {
+    let res = false;
+    if (data) {
+      if (data.length == 0) {
+        res = true;
+      }
+    }
+    else {
+      res = true;
+    }
+    return res;
+  }
+
+  ngOnInit() {
+    
+    this.basicInfService.getOne(this.employeeId).subscribe((res: BasicEmployee) => {
+      console.log(res);
+      this.basicInfo = res;
+    });
+    this.academicService.getOne(this.employeeId).subscribe((res: Academic) => {
+      console.log(res);
+      this.academicInfo = res;
+    });
   }
 
 }
