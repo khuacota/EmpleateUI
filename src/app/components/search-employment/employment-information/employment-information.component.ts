070a524f4dd5c782f7b34f30c9a0ac954580b637
Beleenus <ni_rc@outlook.com>
2018-12-04 09:11:34 -0400

    fix show offer

--- src/app/components/search-employment/employment-information/employment-information.component.ts
+++ src/app/components/search-employment/employment-information/employment-information.component.ts
@@ -2,6 +2,8 @@ import { Component, OnInit } from '@angular/core';
 import { FormBuilder, Validators, FormGroup, FormControl } from '@angular/forms';
 import { JobOfferService } from '../../../services/jobOffer/job-offer.service';
 import { MatExpansionModule } from '@angular/material/expansion';
+import { ActivatedRoute, Router, ParamMap } from '@angular/router';
+import { switchMap } from 'rxjs/operators';
 
 
 @Component({
@@ -10,10 +12,28 @@ import { MatExpansionModule } from '@angular/material/expansion';
   styleUrls: ['./employment-information.component.css']
 })
 export class EmploymentInformationComponent implements OnInit {
-
-  constructor() { }
+  offerId: string;
+  offer: any;
+  employeeId: number;
+  constructor(private route: ActivatedRoute, private router: Router, private jobService: JobOfferService) {
+    this.employeeId = 1;
+    this.offerId = '1';
+    this.route.paramMap.pipe(
+      switchMap((params: ParamMap) =>
+        this.offerId = params.get('id')
+      )
+    );
+    this.jobService.getOne(this.offerId).subscribe(res => {
+      this.offer = res;
+    });
+  }
 
   ngOnInit() {
+  }
+
+  postulate() {
+    let body = { OfferId: this.offerId, EmployeeId: this.employeeId };
+    this.jobService.postulate(body).subscribe();
   }
 
 }
