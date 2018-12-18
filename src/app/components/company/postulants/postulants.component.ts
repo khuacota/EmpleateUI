5d09313a405e1743f8207debf92f9b0f148d7b7c
farzor520 <favi.guerra@gmail.com>
2018-12-18 18:54:31 -0400

    vista3

--- src/app/components/company/postulants/postulants.component.ts
+++ src/app/components/company/postulants/postulants.component.ts
@@ -1,7 +1,8 @@
 import { Component, OnInit } from '@angular/core';
-import { ActivatedRoute, Router, ParamMap } from '@angular/router';
-import { switchMap } from 'rxjs/operators';
-import { MatSnackBar } from '@angular/material';
+import { BasicInformationService } from '../../../services/employee/basic-information.service';
+import { BasicEmployee } from '../../../models/basicEmployee';
+import { FormGroup, Validators, FormBuilder } from '@angular/forms';
+import { OccupationEmp } from '../../../models/occupationEmp';
 
 @Component({
   selector: 'app-postulants',
@@ -9,15 +10,35 @@ import { MatSnackBar } from '@angular/material';
   styleUrls: ['./postulants.component.css']
 })
 export class PostulantsComponent implements OnInit {
-  offerId: string;
-  offer: any;
-  employeeId: number;
-  employee: any;
-
-  constructor() {
+  private proffesionals: Array<BasicEmployee>;
+  private error: boolean;
+  private searchForm: FormGroup;
+  public notfound: boolean;
+  constructor(private serviceEmployee: BasicInformationService, private _formBuilder: FormBuilder) {
+    this.error = false;
+    this.notfound = true;
+    let empp = new BasicEmployee();
+    empp.Occupations = [new OccupationEmp(), new OccupationEmp(),
+    new OccupationEmp()];
+    this.proffesionals = [];
+    this.searchForm = this._formBuilder.group({
+      Searched: ['', Validators.compose([
+        Validators.required
+      ])]
+    });
   }
 
   ngOnInit() {
   }
+  searchEmployees() {
+    this.serviceEmployee.getFilterEmployees(this.searchForm.value.Searched).subscribe((res: Array<BasicEmployee>) => {
+      this.proffesionals = res;
+      if (this.proffesionals.length == 0) {
+        this.notfound = true;
+      }
+    }, error => {
+    });
+  }
 
 }
+
