379d2edddc255f1e9304cbfd3701bcd130c956a2
root <draykoes@gmail.com>
2018-12-18 15:55:58 -0400

    without BE

--- src/app/components/company/home-company/home-company.component.ts
+++ src/app/components/company/home-company/home-company.component.ts
@@ -1,6 +1,9 @@
 import { Component, OnInit } from '@angular/core';
 import {MatExpansionModule} from '@angular/material/expansion';
 import { Router, RouterModule, Routes } from '@angular/router';
+import { JobOfferService } from '../../../services/jobOffer/job-offer.service';
+import { AuthService } from '../../../services/auth/auth.service';
+import { User } from '../../../models/user';
 
 @Component({
   selector: 'app-home-company',
@@ -11,13 +14,28 @@ export class HomeCompanyComponent implements OnInit {
 
   error: Boolean;
   offers: Array<any>;
+  companyName: string;
 
-  constructor(public router: Router,) {
+  constructor(public router: Router, private service: JobOfferService) {
   	this.error = false;
-  	this.offers = [];
+  	this.offers = [];  
+    this.companyName = "Jala";     
   }
 
   ngOnInit() {
+    this.searchEmployment();
+  }
+
+  searchEmployment() {
+    this.service.getOffersByCompany(this.companyName).subscribe(
+      response => {
+        this.offers = response;
+        this.error = this.offers.length == 0;        
+        }, error => {
+        this.offers = [];
+        this.error = true;        
+      }
+    );
   }
 
 }
